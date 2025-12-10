52876b1769803de9d9641ca2c7d18fd06abd5c95
thiagomoslavacz <thiagomoslavacz@gmail.com>
2025-12-10 12:12:04 -0300

    Navbar and Bio / NavBar JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 console.log("Hello World!");
+
+/*
+new Typed("#typed", {
+  strings: ["I develop websites.", "And games too", "Passionate for coffee"],
+  typeSpeed: 50,
+  backSpeed: 30,
+  loop: true,
+});
+*/
+
+const icons = document.querySelectorAll(".icon-wrapper");
+const pages = document.querySelectorAll(".page");
+
+icons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    icons.forEach((i) => i.classList.remove("selected"));
+    icon.classList.add("selected");
+
+    const target = icon.dataset.section;
+
+    pages.forEach((page) => {
+      page.classList.remove("active");
+      if (page.classList.contains(target)) {
+        page.classList.add("active");
+      }
+    });
+  });
+});
